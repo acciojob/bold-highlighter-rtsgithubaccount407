af0d223007ce15a4b60f372ddf09e974265ed81f
Rohit Sharma <123295893+rtsgithubaccount407@users.noreply.github.com>
2023-06-23 15:21:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function highlight() {
     //Write your code here
+	let boldwords = document.getElementByTagName('strong');// select tag name
+	for(let i=0; i<boldwords.length; i++){
+		boldwords[i].style.color = 'green';
+	}
+	
 
 
 }
@@ -7,6 +12,9 @@ function highlight() {
 
 function return_normal() {
     //Write your code here
-
+	let boldWords = document.getElementByTagName('strong');
+	for(let i=0;i<boldWords.length; i++){
+		boldWords[i].style.color = 'black';
+	}
     
 }
