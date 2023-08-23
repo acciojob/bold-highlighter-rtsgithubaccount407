0400a60aa8b19efd0f81c99beee4634bbef1ffac
Rohit Sharma <123295893+rtsgithubaccount407@users.noreply.github.com>
2023-08-23 15:39:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
+ 
 function highlight() {
     //Write your code here
-	let boldwords = document.getElementByTagName('strong');// select tag name
-	for(let i=0; i<boldwords.length; i++){
-		boldwords[i].style.color = 'green';
-	}
-	
-
-
+  var boldWords = document.querySelectorAll('strong')
+  boldWords.forEach(function(word) {
+    word.style.color = 'green';
+  });
+}
+function return_normal() {
+   var boldWords = document.querySelectorAll('strong')
+  boldWords.forEach(function(word) {
+    word.style.color = 'black';
+  });
 }
 
 
